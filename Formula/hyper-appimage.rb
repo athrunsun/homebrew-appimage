841ae97ee160535682fe2b24aa84c581ee46a2ab
okampfer <okampfer@gmail.com>
2018-04-21 09:48:35 +0800

    Upgrade hyper-appimage to version 2.0.0

--- Formula/hyper-appimage.rb
+++ Formula/hyper-appimage.rb
@@ -1,9 +1,9 @@
 class HyperAppimage < Formula
     desc "A terminal built on web technologies."
-    homepage "https://hyper.is"
-    version "1.4.8"
+    homepage "https://hyper.is  https://github.com/zeit/hyper"
+    version "2.0.0"
     url "https://github.com/zeit/hyper/releases/download/#{version}/hyper-#{version}-x86_64.AppImage"
-    sha256 "e116416ee153e1b6db7edfd74071c60be2d67a943168b0e429eb3aee740d5fc7"
+    sha256 "61ef1d6aec92956c62ea94c51b2e5f7f30e832968998f659361366a4f73c7054"
 
     bottle :unneeded
     cxxstdlib_check :skip
