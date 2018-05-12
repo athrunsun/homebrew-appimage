b9f839358a88819272e2a92b78ed985ad80898cd
okampfer <okampfer@gmail.com>
2018-05-12 08:39:16 +0800

    Fix url for vnote-appimage.

--- Formula/vnote-appimage.rb
+++ Formula/vnote-appimage.rb
@@ -2,7 +2,7 @@ class VnoteAppimage < Formula
     desc "A Vim-inspired note-taking application that knows programmers and Markdown better."
     homepage "https://tamlok.github.io/vnote"
     version "1.16"
-    url "https://github.com/tamlok/vnote/releases/download/v#{version}/VNote_x86_64_#{version}.AppImage"
+    url "https://github.com/tamlok/vnote/releases/download/v#{version}/VNote-#{version}-x86_64.AppImage"
     sha256 "2a88701d5fd7e4bd8ff596e92553bc21b89a7b0be357ffd41df0b6cc915be79e"
 
     bottle :unneeded
