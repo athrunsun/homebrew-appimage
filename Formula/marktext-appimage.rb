69a1c7d176704f2093948c5106d40be20d4e885d
okampfer <okampfer@gmail.com>
2018-05-29 15:59:38 +0800

    Add new formula, marktext, version 0.10.21

---
+++ Formula/marktext-appimage.rb
@@ -0,0 +1,22 @@
+class MarktextAppimage < Formula
+    desc "Next generation markdown editor, running on platforms of MacOS Windows and Linux."
+    homepage "https://github.com/marktext/marktext  https://marktext.github.io/website/"
+    version "0.10.21"
+
+    url "https://github.com/marktext/marktext/releases/download/v#{version}/marktext-#{version}-x86_64.AppImage"
+    sha256 "8fbd179a453173032ea553a77442bc5d414d58ae13427b62210b2f833d2bb045"
+
+    bottle :unneeded
+    cxxstdlib_check :skip
+
+    def install
+        prefix.install Dir["*"]
+        chmod(0755, "#{prefix}/marktext-#{version}-x86_64.AppImage")
+        bin.install_symlink("#{prefix}/marktext-#{version}-x86_64.AppImage" => "marktext")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "marktext".
+        EOS
+    end
+end
