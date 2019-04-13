47dcc58dd670cef6d9124d832bd0eec59dd5fadc
0x639d1dec74 <JuPlutonic@users.noreply.github.com>
2019-04-12 22:50:04 -1100

    Update - old ver - Not applicable/segfault
    marktext-0.14.0-x86_64.AppImage

--- Formula/marktext-appimage.rb
+++ Formula/marktext-appimage.rb
@@ -1,10 +1,10 @@
 class MarktextAppimage < Formula
     desc "Next generation markdown editor, running on platforms of MacOS Windows and Linux."
     homepage "https://github.com/marktext/marktext"
-    version "0.10.21"
+    version "0.14.0"
 
     url "https://github.com/marktext/marktext/releases/download/v#{version}/marktext-#{version}-x86_64.AppImage"
-    sha256 "8fbd179a453173032ea553a77442bc5d414d58ae13427b62210b2f833d2bb045"
+    sha256 "333b55f78779c8ce93bace15987d546d46d05f4543b90f1b6afdbadf348921d1"
 
     bottle :unneeded
     cxxstdlib_check :skip
